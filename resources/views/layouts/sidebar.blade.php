dd61262fb3fd411b7b2029c782a4dfab07e4c878
projectKangcutz <rizkyindranurbayu@gmail.com>
2025-08-14 12:10:35 +0700

    dropdown sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,27 +1,39 @@
-    <ul class="sidebar-nav" id="sidebar-nav">
+<ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link " href="/dashboard">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
+  <li class="nav-item">
+    <a class="nav-link" href="/dashboard">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
 
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-toggle="collapse" href="#menu-kk" role="button" aria-expanded="false" aria-controls="menu-kk">
+      <i class="bi bi-pencil-square"></i>
+      <span>Kartu Keluarga</span>
+      <i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul class="collapse" id="menu-kk" data-bs-parent="#sidebar-nav">
       <li class="nav-item">
-        <a class="nav-link " href="/kartukeluargas">
-          <i class="bi bi-pencil-square"></i>
-          <span>Kartu Keluarga</span>
+        <a href="/kartukeluargas" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <span>List Kartu Keluarga</span>
         </a>
       </li>
-
       <li class="nav-item">
-        <a class="nav-link " href="/dashboard/laporan">
-          <i class="bi bi-archive-fill"></i>
-          <span>Laporan</span>
+        <a href="/kartukeluargas/create" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <span>Create Kartu Keluarga</span>
         </a>
       </li>
-
-
     </ul>
+  </li><!-- End Kartu Keluarga Nav -->
 
+  <li class="nav-item">
+    <a class="nav-link" href="/dashboard/laporan">
+      <i class="bi bi-archive-fill"></i>
+      <span>Laporan</span>
+    </a>
+  </li>
 
+</ul>
